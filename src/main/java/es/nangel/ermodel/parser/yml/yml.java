a2b58f997042d7162ba6b947e7433db185d74243
Ángel Luis López Monterroso <angel9484@gmail.com>
2015-10-22 21:58:39 +0200

    Skeleton of module.

---
+++ src/main/java/es/nangel/ermodel/parser/yml/yml.java
@@ -0,0 +1,11 @@
+package es.nangel.ermodel.parser.yml;
+
+import es.nangel.ermodel.parser.Parser;
+
+import java.lang.reflect.ParameterizedType;
+
+/**
+ * Created by nangel on 22/10/15.
+ */
+public class yml implements Parser {
+}
